cfc3252411174629861d3f1da103143b4ea4829d
John Doe <johndoe@example.com>
2026-01-05 17:23:39 +0700

    week-07

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class App {
   protected readonly title = signal('week-07');
---
+++ src/app/example/components/display-contact/display-contact.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-contact',
+  imports: [],
+  templateUrl: './display-contact.html',
+  styleUrl: './display-contact.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DisplayContact {
+
+}
